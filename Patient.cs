13f707ed59281b4f73a08d2e298b337f99a751c2
fatimah1996 <eshbelia123@gmail.com>
2019-02-23 12:04:19 +0200

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Globaldev.Models
+{
+    public class Patient
+    {
+        public int ID { get; set; }
+        public string fName { get; set; }
+        public string mName { get; set; }
+        public string lName { get; set; }
+        public DateTime DOB ;
+        public string gender { get; set; }
+        public string email { get; set; }
+        public string lastCheck { get; set; }
+        public string status { get; set; }
+        public string Active { get; set; }
+        public DateTime CreationDate;
+        public string CreatedBy { get; set; }
+    
+    }
+}
---
+++ PatientController.cs
@@ -0,0 +1,28 @@
+﻿using Globaldev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Globaldev.Controllers
+{
+    public class PatientController : Controller
+    {
+        // GET: Patient
+        public ActionResult Index()
+        {
+            var patientList = new List<Patient>{
+                            new Patient() { ID= 1 , fName = "Fatimah", mName = "John",
+                                lName = "Al-khateeb"  ,DOB = new DateTime(1995, 1, 13) , gender="Female" ,
+                                email = "fatimah@gmail.com" , lastCheck ="2:00pm" , status ="not in hospital" ,
+                                Active ="true"  , CreationDate = new DateTime(2000, 1, 13) , CreatedBy="Ahmad"  } ,
+                            
+                      
+                        };
+            // Get the students from the database in the real application
+
+            return View(patientList);
+        }
+    }
+}
